7469cc9be1771693af78e27e439f4f50f4579e47
Aimee MacDonald <aimeelmacdonald@gmail.com>
2019-02-28 16:38:18 +0200

    Admin Pages

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
 const express = require("express");
 const app = express();
+
+app.use(express.static(__dirname + "/static"));
 
 app.listen(8080, console.log("Server Listening on Port: " + 8080));
 
 app.get("/", (req, res) => {
   res.status(200).sendFile(__dirname + "/views/index.html");
+});
+
+app.get("/admin", (req, res) => {
+  res.redirect("/admin/register");
+});
+
+app.get("/admin/register", (req, res) => {
+  res.status(200).sendFile(__dirname + "/views/admin-register.html");
+});
+
+app.get("/admin/login", (req, res) => {
+  res.status(200).sendFile(__dirname + "/views/admin-login.html");
 });
