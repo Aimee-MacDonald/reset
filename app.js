8330b5700ec9f3a04bc704d74dff2b84fa0b1ecd
Aimee MacDonald <aimeelmacdonald@gmail.com>
2019-02-28 16:46:24 +0200

    Heroku Configuration

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.use(express.static(__dirname + "/static"));
 
-app.listen(8080, console.log("Server Listening on Port: " + 8080));
+app.listen(process.env.PORT || 8080);
 
 app.get("/", (req, res) => {
   res.status(200).sendFile(__dirname + "/views/index.html");
