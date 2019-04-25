b59546c87838acc93eb653e127c30368e8ed7112
Aimee MacDonald <aimeelmacdonald@gmail.com>
2019-04-25 20:41:45 +0200

    Admin Authentication Stand-in Endpoints

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
 app.use(express.static(__dirname + "/static"));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 app.listen(process.env.PORT || 8080);
 
@@ -17,8 +23,16 @@ app.get("/admin/register", (req, res) => {
   res.status(200).sendFile(__dirname + "/views/admin-register.html");
 });
 
+app.post("/admin/auth/register", (req, res) => {
+  res.status(200).send("<h1>" + req.body.name + "</h1>");
+});
+
 app.get("/admin/login", (req, res) => {
   res.status(200).sendFile(__dirname + "/views/admin-login.html");
+});
+
+app.post("/admin/auth/login", (req, res) => {
+  res.status(200).send("<h1>" + req.body.email + "</h1>");
 });
 
 app.get("/admin/pages", (req, res) => {
