3a24825e44cc5ef9ce5ab034a876b4425ee73f1c
Aimee MacDonald <aimeelmacdonald@gmail.com>
2019-04-27 23:23:01 +0200

    Password Encryption

--- dbmodels/siteAdmin.js
+++ dbmodels/siteAdmin.js
@@ -1,11 +1,25 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const bcrypt = require("bcryptjs");
 
 var schema = new Schema({
   username: {type: String, required: true},
   email: {type: String, required: true},
   password: {type: String, required: true},
   homepage: {type: String, required: true}
+});
+
+schema.pre("save", function(callback){
+  var user = this;
+  bcrypt.genSalt(5, function(err, salt){
+    if(err) throw err;
+
+    bcrypt.hash(user.password, salt, function(err, hash){
+      if(err) throw err;
+      user.password = hash;
+      callback();
+    });
+  });
 });
 
 module.exports = mongoose.model("SiteAdmin", schema);
